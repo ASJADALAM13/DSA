b5e3d44de761d481a2e5cfefde6bd42b69addf65
MD ASJAD ALAM <142597782+ASJADALAM13@users.noreply.github.com>
2025-08-31 08:56:16 +0530

    Add files via upload

---
+++ check_palindrome.py
@@ -0,0 +1,21 @@
+num=int(input("Enter no:"))
+
+
+def check_palindrome(num):
+    n=num
+    pald=0
+    while n!=0:
+        ld=n%10
+        pald=(pald*10)+ld
+        n=n//10
+
+    if num==pald:
+        print("Palindrome")
+    else:
+        print("Not A palindrome")
+
+check_palindrome(num)
+
+
+
+
